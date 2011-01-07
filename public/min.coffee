65e8f20dad58912d9cf851de1df6c0bcfb003b1b
dab <dbraband@gmail.com>
2011-01-07 17:22:28 +0100

    Add Google Analytics

--- public/min.coffee
+++ public/min.coffee
@@ -8,3 +8,16 @@ $("form").submit( ->
     )
     return false
 )
+
+# Google Analytics
+_gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-67221-12'])
+_gaq.push(['_trackPageview'])
+->
+    ga = document.createElement('script')
+    ga.type = 'text/javascript'
+    ga.async = true
+    ga.src = 'http://www.google-analytics.com/ga.js'
+    s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(ga, s)
+
--- public/min.js
+++ public/min.js
@@ -1,4 +1,5 @@
 (function() {
+  var _gaq;
   $("form").submit(function() {
     var before, result;
     before = $('#js').val();
@@ -9,5 +10,17 @@
       return result.children('textarea').val(data).focus().select();
     });
     return false;
+  });
+  _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-67221-12']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga, s;
+    ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'http://www.google-analytics.com/ga.js';
+    s = document.getElementsByTagName('script')[0];
+    return s.parentNode.insertBefore(ga, s);
   });
 }).call(this);
