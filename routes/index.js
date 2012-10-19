c3fd0439a47f94ba13b62851af8eb140711f8638
Danilo Braband <d.braband@k-newmedia.de>
2012-10-19 18:31:06 +0200

    Basic POST functionality.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+var uglify_js = require('uglify-js2');
+
 /*
  * GET /
  */
@@ -9,6 +11,15 @@ exports.index = function (req, res) {
  * POST /
  */
 exports.post = function (req, res) {
-
+  minimize_js(req.body.content);
+  //res.render('index', { title: 'Compress Your Javascript' });
 };
+
+
+var minimize_js = function(data) {
+  console.log(data);
+  var result = uglify_js.minify(data, {fromString: true});
+  console.log(result.code);
+  console.log(result.map);
+}
 
